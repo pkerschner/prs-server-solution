815dc51758b826a4ef74bf26d334dc3019c25526
Philip Kerschner <pkerschner@zoominternet.net>
2022-02-18 12:51:10 -0500

    Add project files.

---
+++ prs-server-project/Models/PrsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prs_server_project.Models {
+
+    public class PrsDbContext : DbContext {
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public PrsDbContext(DbContextOptions<PrsDbContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.Entity<User>(e => {
+                e.HasIndex(p => p.Username).IsUnique(true);
+            });
+        }
+    }
+}
---
+++ prs-server-project/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prs_server_project.Models {
+
+    public class User {
+
+        public int Id { get; set; }
+        [Required, StringLength(30)]
+        public string Username { get; set; }
+        [Required, StringLength(30)]
+        public string Password { get; set; }
+        [Required, StringLength(30)]
+        public string Firstname { get; set; }
+        [Required, StringLength(30)]
+        public string Lastname { get; set; }
+        [StringLength(12)]
+        public string Phone { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+        public bool IsReviewer { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User() { }
+    }
+}
